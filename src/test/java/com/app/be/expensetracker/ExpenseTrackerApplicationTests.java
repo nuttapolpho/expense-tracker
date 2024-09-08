7cfd0cf647b7674ac7c369d64e6c8486e0c676c4
notnuttt <nutnutnut12079@hotmail.com>
2024-09-08 12:20:58 +0700

    feat: initial commit

---
+++ src/test/java/com/app/be/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.be.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
